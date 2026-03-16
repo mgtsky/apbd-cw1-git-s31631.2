a24f60623e4e8a4a10c98feb304a9581bc891216
Marharyta Tsyshevska <s31631@pjwstk.edu.pl>
2026-03-16 20:42:09 +0100

    Add user input handling

--- StatisticsApp/StatisticsApp/Program.cs
+++ StatisticsApp/StatisticsApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using StatisticsApp;
+
+
+Console.WriteLine("Enter numbers separated by space:");
+
+string input = Console.ReadLine();
+
+string[] parts = input.Split(" ");
+
+int[] numbers = parts.Select(int.Parse).ToArray();
+
+double avg = StatisticsHelper.CalculateAverage(numbers);
+
+Console.WriteLine("Average: " + avg);
